b732630346da5f658a163bbd3ec01ba69bd67123
Minh Hoang Tuan <minhhtml5@gmail.com>
2017-08-16 09:34:52 +0700

    - Config build properly

--- frameworks/runtime-src/Classes/AppDelegate.cpp
+++ frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -212,3 +212,22 @@ void AppDelegate::applicationWillEnterForeground()
     SimpleAudioEngine::getInstance()->resumeAllEffects();
 #endif
 }
+
+
+
+
+static u_long myNextRandom = 1;
+double atof(const char *nptr)
+{
+    return (strtod(nptr, NULL));
+}
+
+int rand(void)
+{
+    return (int)((myNextRandom = (1103515245 * myNextRandom) + 12345) % ((u_long)RAND_MAX + 1));
+}
+
+void srand(u_int seed)
+{
+    myNextRandom = seed;
+}
